4dceb1c288d7233732bde9720a5be960db9c360e
Nery93 <71045052+Nery93@users.noreply.github.com>
2021-08-17 16:05:52 +0100

    Add files via upload

---
+++ Urna eletronica/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'./img/v1.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'./img/v2.jpg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '17',
+                nome: 'Beltrano',
+                partido: 'PP',
+                fotos: [
+                    {url:'./img/cp1.jpg', legenda:'Prefeito'},
+                    {url:'./img/cp2.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'PC',
+                partido: 'Metalurgico',
+                fotos: [
+                    {url:'./img/cp3.jpg', legenda:'Prefeito'},
+                    {url:'./img/cp4.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+
+   
+]
